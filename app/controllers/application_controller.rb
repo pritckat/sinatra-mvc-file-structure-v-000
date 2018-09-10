aaa2c0cb5e33aa1e5fb40246721bd095e4898a92
Katrina Pritchard <pritckat@gmail.com>
2018-09-10 16:00:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,3 +9,15 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 end
+
+class Dog 
+
+  attr_accessor :name, :age
+  attr_reader :breed
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+end
